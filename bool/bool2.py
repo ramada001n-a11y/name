f8610efe536022d867d6effb22c1950836486059
Ramazan <ramada001@gmail.com>
2026-02-10 17:52:52 +0500

    Initial commit

---
+++ bool/bool2.py
@@ -0,0 +1,7 @@
+a = True
+i =0 
+while a == True:
+    print(i)
+    if i == 10:
+        
+        a = False
---
+++ loop/for.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = list(map(int, input().split()))
+c =0
+for i in range(0,a):    
+    if b[i] > 0:
+        c += 1
+print(c)
