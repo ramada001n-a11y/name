82760dcce858ea297b1480490e655fe2932587b1
Ramazan <ramada001@gmail.com>
2026-02-10 16:06:16 +0500

    loop max number

---
+++ Practice2/if else/lopp.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = 1
+
+c = ((2*b + (a -1))*a)/2
+ 
+print(c)
--- p.py
+++ p.py
@@ -1,1 +1,8 @@
-print(1)
+n = int(input())
+l = int(input())
+r = int(input())
+arr = list(map(int, input().split()))
+arr.sort(reverse=True)
+
+for i in range(l,-1):
+    print(arr[i], end=' ')
